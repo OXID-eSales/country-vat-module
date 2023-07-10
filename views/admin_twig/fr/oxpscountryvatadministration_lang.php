4cc367fed5f2d6e9759444f39336920e044badde
Gabriel Peleskei <gp@itholics.de>
2023-07-10 13:13:54 +0000

    Added trig admin translations

---
+++ views/admin_twig/fr/oxpscountryvatadministration_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$sLangName = 'English';
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = [
+    'charset'                                             => 'UTF-8',
+    'OXPSCOUNTRYVATADMINISTRATION_COUNTRY_MAIN_VAT'       => 'TVA spécifique au pays',
+    'OXPSCOUNTRYVATADMINISTRATION_COUNTRY_VAT_INPUT'      => 'TVA spécifique',
+    'OXPSCOUNTRYVATADMINISTRATION_COUNTRY_ARTICLE'        => 'Pays spécifique',
+    'OXPSCOUNTRYVATADMINISTRATION_COUNTRY_COUNTRY'        => 'Pays',
+    'OXPSCOUNTRYVATADMINISTRATION_COUNTRYVAT_SAVE'        => 'Save',
+];
